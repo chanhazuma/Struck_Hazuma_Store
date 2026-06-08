63eb15266e45ce7bec78507f360dedc7d22205c2
chanhazuma <chanhazuma@gmail.com>
2026-06-08 10:24:41 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+// Service Worker Sederhana untuk PWA
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Terinstal');
+});
+
+self.addEventListener('fetch', (e) => {
+    // Membiarkan request berjalan normal
+});
